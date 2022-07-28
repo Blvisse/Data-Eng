8bba8f66861d6a390dffde91336426a8c080cb5e
Blvisse <blaisepke@gmail.com>
2022-07-28 13:23:12 +0300

    Added data sanity check

---
+++ whiskey-retail-store/datawarehouse_test.py
@@ -0,0 +1,56 @@
+'''
+This script carries out unit testing on data fetched from the datawarehouse.
+
+'''
+import pandas as pd
+import unittest
+
+
+class DataTest(unittest.TestCase):
+    
+    
+    
+
+    
+    def test_prices(self):
+        data=r'C:\Users\blais\Desktop\Masters\ML Track\Data-Eng\whiskey-retail-store\data\dwh_fact.csv'
+        data=pd.read_csv(data)
+        col='Alcohol_Price'
+        
+        
+        
+        # for col in date_cols:
+            
+        self.assertEqual(data[col].dtype, 'float64', "Failed date test for column:")
+            
+        
+        
+        # try:
+        #     test_dates()
+        #     print("Tests Completed Successfully ...")
+        # except Exception as e:
+        #     print ("Error occurred while running tests: {}".format(e))
+
+
+# def test_dates(data: pd.DataFrame,datecols: list):
+#     '''
+#     This function tests the dates in the data warehouse.
+#     '''
+#     for col in datecols:
+#         assert data[col].dtype == 'datetime64[ns]', "Failed date test for column: {}".format(col)
+        
+        
+        
+
+if __name__ == '__main__':
+    data='../data/whisky_data.csv'
+    date_cols=['Product_name']    
+    
+    unittest.main()
+    # try:
+    #     test_dates()
+    #     print("Tests Completed Successfully ...")
+    # except Exception as e:
+    #     print ("Error occurred while running tests: {}".format(e))
+    
+    
